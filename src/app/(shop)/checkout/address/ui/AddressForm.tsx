94ca08e9e86db711540df36cfb4c7f04891310e5
jose angel <jose.tamaroni.rodriguez@gmail.com>
2025-07-21 19:46:48 -0300

    correccion warnings

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -48,7 +48,7 @@ export const AddressForm = ({ countries, userStoreAddress = {} }: Props) => {
         if (address.firstName) {
             reset(address);
         }
-    }, [])
+    }, [address, reset]);
 
 
     const onSubmit = async (data: FormInputs) => {
